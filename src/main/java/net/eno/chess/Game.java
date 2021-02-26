43c47d91e1a0bd1b59d27fc12b46b6be02014934
jeong-inho <derosatam76@gmail.com>
2021-02-26 15:37:29 +0900

    Add move function in Game class Game class에 프로그램을 시작하면 체스판에 기물을 이동할 수 있도록 move 기능 추가

--- src/main/java/net/eno/chess/Game.java
+++ src/main/java/net/eno/chess/Game.java
@@ -1,31 +1,43 @@
 package net.eno.chess;
 
-import net.eno.pieces.Color;
+import net.eno.pieces.*;
 
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
-        System.out.println("> start를 입력하시면 체스 게임이 시작됩니다.");
+        System.out.println("> start를 입력하면 체스 게임이 시작됩니다.");
+        System.out.println("> 게임이 시작되면 move 명령어를 통해 기물을 이동할 수 있습니다.");
+        System.out.println("> ex) move a1 a2");
+        System.out.println("> end를 입력하면 체스 게임이 종료됩니다.");
+
         Scanner sc = new Scanner(System.in);
-        String command;
-        Board board;
-        boolean check = false;
+        Board board = new Board();
+        boolean isStart = false;
 
         while (true) {
             System.out.print("> ");
-            command = sc.next();
+            String command = sc.nextLine();
 
             if (command.equals("start")) {
                 System.out.println("> 체스 게임을 시작합니다.");
-                board = new Board();
+                isStart = true;
                 board.initialize();
                 System.out.println(board.showBoard(Color.WHITE));
-                check = true;
             }
 
-            if (check && command.equals("end")) {
+            if (isStart && command.startsWith("move")) {
+                String[] splitCommand = command.split(" ");
+                Position sourcePosition = new Position(splitCommand[1]);
+                Position targetPosition = new Position(splitCommand[2]);
+
+                board.movePiece(sourcePosition, targetPosition);
+                System.out.println(board.showBoard(Color.WHITE));
+            }
+
+            if (isStart && command.equals("end")) {
                 System.out.println("> 체스 게임을 종료합니다.");
+                sc.close();
                 break;
             }
         }
