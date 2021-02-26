f24af6df152f265bd6f63aea175b693b1509cd68
jeong-inho <derosatam76@gmail.com>
2021-02-26 15:35:25 +0900

    Create move method in Board class 기물의 위치를 변경하기 위해 위치를 변경할 기물의 좌표와 기물을 놓을 좌료를 입력 받아 기물을 이동하는 move 메소드 생성

--- src/main/java/net/eno/chess/Board.java
+++ src/main/java/net/eno/chess/Board.java
@@ -1,17 +1,11 @@
 package net.eno.chess;
 
-import net.eno.pieces.Color;
-import net.eno.pieces.Piece;
-import net.eno.pieces.PieceType;
-import net.eno.pieces.Position;
+import net.eno.pieces.*;
 
 import static net.eno.utils.StringUtils.appendNewLine;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.*;
 
 
 public class Board {
@@ -20,59 +14,6 @@ public class Board {
 
     public Board() {
         this.board = new ArrayList<>();
-    }
-
-    private void addRank(Rank rank) {
-        this.board.add(rank);
-    }
-
-    public int countTargetPiece(Color color, PieceType pieceType) {
-        return this.board.stream()
-                .mapToInt(rank -> rank.countTargetPiece(color, pieceType))
-                .sum();
-    }
-
-    public Piece findPiece(String position) {
-        Position positionObj = new Position(position);
-        Rank targetRank = this.board.get(positionObj.getRankIndex());
-        return targetRank.findPiece(positionObj.getFileIndex());
-    }
-
-    public void move(String position, Piece piece) {
-        Position positionObj = new Position(position);
-        Rank targetRank = this.board.get(positionObj.getRankIndex());
-        targetRank.move(positionObj.getFileIndex(), piece);
-    }
-
-    public double calculatePoint(Color color) {
-        double point = this.board.stream()
-                .mapToDouble(rank -> rank.calculateRankPoint(color))
-                .sum();
-        double pawnNumber = IntStream.rangeClosed('a', 'h')
-                .mapToDouble(file -> countSameFilePawn((char)file, color))
-                .sum();
-        return point - (pawnNumber / 2);
-    }
-
-    private int countSameFilePawn(char file, Color color) {
-        Piece pawn = Piece.createPiece(color, PieceType.PAWN);
-        int count = (int)IntStream.rangeClosed(1, 8)
-                .mapToObj(rank -> findPiece(String.valueOf(file) + rank))
-                .filter(piece -> piece.equals(pawn))
-                .count();
-        return count > 1 ? count : 0;
-    }
-
-    public List<Piece> sortByPiecePoint(Color color, boolean isAscending) {
-        List<Piece> pieceList = this.board.stream()
-                .map(rank -> rank.getPieceListByColor(color))
-                .flatMap(List::stream)
-                .sorted()
-                .collect(Collectors.toList());
-        if (!isAscending) {
-            Collections.reverse(pieceList);
-        }
-        return pieceList;
     }
 
     public void initialize() {
@@ -91,6 +32,68 @@ public class Board {
         this.board = new ArrayList<>();
         IntStream.range(0, 8)
                 .forEach(i -> addRank(Rank.createOnePieceRank(Color.NOCOLOR, PieceType.NO_PIECE)));
+    }
+
+    private void addRank(Rank rank) {
+        this.board.add(rank);
+    }
+
+    public Piece findPiece(Position position) {
+        Rank targetRank = findRank(position);
+        return targetRank.findPiece(position);
+    }
+
+    private Rank findRank(Position position) {
+        return this.board.get(position.getRankIndex());
+    }
+
+    public void movePiece(Position sourcePosition, Position targetPosition) {
+        Piece sourcePiece = findPiece(sourcePosition);
+        setPiece(targetPosition, sourcePiece);
+        Piece blankPiece = Piece.createPiece(Color.NOCOLOR, PieceType.NO_PIECE);
+        setPiece(sourcePosition, blankPiece);
+    }
+
+    public void setPiece(Position position, Piece piece) {
+        Rank targetRank = findRank(position);
+        targetRank.setPiece(position, piece);
+    }
+
+    public double calculatePoint(Color color) {
+        double point = this.board.stream()
+                .mapToDouble(rank -> rank.calculateRankPoint(color))
+                .sum();
+        double pawnNumber = IntStream.rangeClosed('a', 'h')
+                .mapToDouble(file -> countSameFilePawn((char)file, color))
+                .sum();
+        return point - (pawnNumber / 2);
+    }
+
+    private int countSameFilePawn(char file, Color color) {
+        Piece pawn = Piece.createPiece(color, PieceType.PAWN);
+        int count = (int)IntStream.rangeClosed(1, 8)
+                .mapToObj(rank -> findPiece(new Position(String.valueOf(file) + rank)))
+                .filter(piece -> piece.equals(pawn))
+                .count();
+        return count > 1 ? count : 0;
+    }
+
+    public int countTargetPiece(Color color, PieceType pieceType) {
+        return this.board.stream()
+                .mapToInt(rank -> rank.countTargetPiece(color, pieceType))
+                .sum();
+    }
+
+    public List<Piece> sortByPiecePoint(Color color, boolean isAscending) {
+        List<Piece> pieceList = this.board.stream()
+                .map(rank -> rank.getPieceListByColor(color))
+                .flatMap(List::stream)
+                .sorted()
+                .collect(Collectors.toList());
+        if (!isAscending) {
+            Collections.reverse(pieceList);
+        }
+        return pieceList;
     }
 
     public String showBoard(Color color) {
